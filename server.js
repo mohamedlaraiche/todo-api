ed0ce3ad5f53b20f08435745dee87ddafe39eb0e
moa <moa@moa.com>
2023-03-18 16:51:16 +0100

    backend

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://todo-client-navy.vercel.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
