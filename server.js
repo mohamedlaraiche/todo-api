afeba0a7c17556c7200f1e0e1fba70486e8ea73c
moa <molaraiche@molaraiche.com>
2023-06-15 09:28:44 +0100

    updates

--- server.js
+++ server.js
@@ -7,17 +7,13 @@ const app = express();
 
 connectDB();
 
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
-
 app.use("/api/todos", router);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
